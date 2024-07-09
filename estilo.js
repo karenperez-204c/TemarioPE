a5577dccab445da28434a9b9df2692183f4d15ec
Karen Perez Ortiz <230110326@itsoeh.edu.mx>
2024-07-09 15:35:49 -0600

    Se agrego los ultimos cambios

--- estilo.js
+++ estilo.js
@@ -5,4 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     toggleButton.addEventListener('click', function() {
         indexNav.classList.toggle('show');
     });
+
+    document.addEventListener('click', function(event) {
+        const isClickInsideButton = toggleButton.contains(event.target);
+        if (!isClickInsideButton) {
+            indexNav.classList.remove('show');
+        }
+    });
+    
+    indexNav.addEventListener('click', function(event) {
+        event.stopPropagation();
+    });
 });
